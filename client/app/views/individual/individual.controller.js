d75f46f89f2496189971cd62a1b0313000ef57b6
Mark Hoffmeyer <mhoffmeyer@deloitte.com>
2015-12-22 10:59:04 -0500

    Adding d3 interface, remove timeline view, add  to for fullpage so d3 can init after

--- client/app/views/individual/individual.controller.js
+++ client/app/views/individual/individual.controller.js
@@ -4,7 +4,7 @@ angular.module('epaRfiApp')
   .controller('IndividualCtrl', function ($scope, appConfig, resourceService, stateManager) {
   	var vm = this;
 
-    vm.resourceData = null; // data for d3, it is set in the init()
+    vm.resourceData = []; // data for d3, it is set in the init()
 
     // watch for changes in the selected state
     $scope.$watch(function() {
@@ -23,6 +23,7 @@ angular.module('epaRfiApp')
       var state = stateManager.getSelectedState();
       resourceService.getAllResourcesForState(state, 2013, 'capita').then(function(response) {
         vm.resourceData = response.data;
+        vm.d3api.refresh();
       });
     }
 
@@ -33,36 +34,7 @@ angular.module('epaRfiApp')
     vm.energyTypeClick = function(d) {
       vm.showLegend = false;
       vm.selectedEnergyData = d;
-      console.log(vm.showLegend);
       $scope.$apply();
     };
-
-    var ENERGY_TYPES = appConfig.ENERGY_TYPES;
-    vm.circleData = [
-      {
-        radius: 100,
-        type: ENERGY_TYPES.MOTOR_GASOLINE
-      },
-      {
-        radius: 50,
-        type: ENERGY_TYPES.NATURAL_GAS
-      },
-      {
-        radius: 35,
-        type: ENERGY_TYPES.COAL
-      },
-      {
-        radius: 20,
-        type: ENERGY_TYPES.NUCLEAR
-      },
-      {
-        radius: 15,
-        type: ENERGY_TYPES.WIND
-      },
-      {
-        radius: 65,
-        type: ENERGY_TYPES.SOLAR
-      }
-    ];
 
   });
