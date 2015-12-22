e8f0671a150c89b67a355befea552df2ba8360b2
Kristian <kristiantran@gmail.com>
2015-12-22 11:58:31 -0500

    Initial Commits for the Nation View

--- client/app/views/nation/nation.controller.js
+++ client/app/views/nation/nation.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('epaRfiApp')
-  .controller('NationCtrl', function (resourceService) {
+  .controller('NationCtrl', function (appConfig, resourceService) {
     
     var vm = this;
 
@@ -16,5 +16,45 @@ angular.module('epaRfiApp')
 	      vm.resourceData = response.data;
 	    });
 	  }
+
+	 	/**
+		 * Generic Functions for d3
+		 */
+		vm.showLegend = true;
+
+	  vm.energyTypeClick = function(d) {
+	    vm.showLegend = false;
+	    vm.selectedEnergyData = d;
+	    console.log(vm.showLegend);
+	    $scope.$apply();
+	  };
+
+		var ENERGY_TYPES = appConfig.ENERGY_TYPES;
+	  vm.circleData = [
+	    {
+	      radius: 100,
+	      type: ENERGY_TYPES.MOTOR_GASOLINE
+	    },
+	    {
+	      radius: 50,
+	      type: ENERGY_TYPES.NATURAL_GAS
+	    },
+	    {
+	      radius: 35,
+	      type: ENERGY_TYPES.COAL
+	    },
+	    {
+	      radius: 20,
+	      type: ENERGY_TYPES.NUCLEAR
+	    },
+	    {
+	      radius: 15,
+	      type: ENERGY_TYPES.WIND
+	    },
+	    {
+	      radius: 65,
+	      type: ENERGY_TYPES.SOLAR
+	    }
+	  ];
 
   });
