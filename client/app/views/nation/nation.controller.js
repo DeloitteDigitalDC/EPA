a1daf1d19910101c22c3d3051b0d7706de2c3e06
Kristian <kristiantran@gmail.com>
2015-12-22 12:40:12 -0500

    Adding d3 and us map to nation view

--- client/app/views/nation/nation.controller.js
+++ client/app/views/nation/nation.controller.js
@@ -4,6 +4,7 @@ angular.module('epaRfiApp')
   .controller('NationCtrl', function (appConfig, resourceService) {
     
     var vm = this;
+    vm.resourceData = []; // data for d3
 
     init();
 
@@ -12,12 +13,16 @@ angular.module('epaRfiApp')
 		 * @return {Object}
 		 */
 	  function init() {
-	    resourceService.getAllResourcesForState('United States', 2013).then(function(response) {
+	    resourceService.getAllResourcesForState('United States', 2013, 'capita').then(function(response) {
 	      vm.resourceData = response.data;
+	      vm.btuTotal = _.sum(response.data, function(resource) {
+	        return resource.usage;
+	      });
+	      vm.d3api.refresh();
 	    });
 	  }
 
-	 	/**
+		/**
 		 * Generic Functions for d3
 		 */
 		vm.showLegend = true;
@@ -25,36 +30,7 @@ angular.module('epaRfiApp')
 	  vm.energyTypeClick = function(d) {
 	    vm.showLegend = false;
 	    vm.selectedEnergyData = d;
-	    console.log(vm.showLegend);
 	    $scope.$apply();
 	  };
-
-		var ENERGY_TYPES = appConfig.ENERGY_TYPES;
-	  vm.circleData = [
-	    {
-	      radius: 100,
-	      type: ENERGY_TYPES.MOTOR_GASOLINE
-	    },
-	    {
-	      radius: 50,
-	      type: ENERGY_TYPES.NATURAL_GAS
-	    },
-	    {
-	      radius: 35,
-	      type: ENERGY_TYPES.COAL
-	    },
-	    {
-	      radius: 20,
-	      type: ENERGY_TYPES.NUCLEAR
-	    },
-	    {
-	      radius: 15,
-	      type: ENERGY_TYPES.WIND
-	    },
-	    {
-	      radius: 65,
-	      type: ENERGY_TYPES.SOLAR
-	    }
-	  ];
 
   });
