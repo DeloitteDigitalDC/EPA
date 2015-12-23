aa391c4ffac10c61702647c60ebebd0ace24f42d
Mark Hoffmeyer <mhoffmeyer@gmail.com>
2015-12-23 14:09:14 -0500

    ngInclude SVG for easier CSS styling

--- client/components/detailBox/detailBox.directive.js
+++ client/components/detailBox/detailBox.directive.js
@@ -8,6 +8,9 @@
       },
       templateUrl: 'components/detailBox/detailBox.html',
       link    : function (scope, element) {
+        scope.$watch('energyData', function(newVal) {
+          scope.svgPath = '../assets/images/' + newVal.energyType.abbr + '.svg';
+        });
 
       }
     };
