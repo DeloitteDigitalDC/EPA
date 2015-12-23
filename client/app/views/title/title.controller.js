27395b8851f264a6db4dcf4761c55c3d349e1d7b
Kristian <kristiantran@gmail.com>
2015-12-23 15:16:19 -0500

    Removing DC from the State Comparison

--- client/app/views/title/title.controller.js
+++ client/app/views/title/title.controller.js
@@ -8,7 +8,9 @@ angular.module('epaRfiApp')
   vm.disableDownButton = true;
 
   resourceService.getResourceList().then(function(result) {
-    vm.stateArray = _.reject(result.data.States, {'name': 'United States'});
+    vm.stateArray = _.reject(result.data.States, function(item) {
+      return (item.name === 'United States') || (item.name === 'District of Columbia');
+    });
   });
 
   vm.stateChangedEvent = function(newState) {
